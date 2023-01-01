cbb335176e267d5c2eb97589d46f9d4c8e492b69
NathanKlump <98673875+NathanKlump@users.noreply.github.com>
2022-12-31 22:22:01 -0500

    add create grid

--- src/App.js
+++ src/App.js
@@ -1,7 +1,58 @@
+import { useState, useRef } from "react";
+
 function App() {
+  const [arraySize, setArraySize] = useState(1);
+  const [grid, setGrid] = useState([]);
+  const canvasRef = useRef(null);
+
+
+
+
+const displayGrid = () => {
+  for(let i; i < grid.length; i++){
+  }
+}
+
+const createGrid = () => {
+  const array = [];
+  for (let i = 0; i < arraySize; i++) {
+    array[i] = [];
+    for (let j = 0; j < arraySize; j++) {
+      array[i][j] = 
+        {
+          active: "false"
+        };
+    }
+  }
+  setGrid(array);
+}
+
+const handleClick = () => {
+  createGrid();
+  displayGrid();
+}
   return (
-    <div className="App">
-      <p className="text-color-blue">bruh</p>
+    <div className="justify-center mx-auto">
+      <div className="flex items-center justify-between mx-auto">
+        <p className="text-6xl font-open-sans">Union find</p>
+      </div>
+        <input 
+        type="number" 
+        min="1" 
+        max="100" 
+        className="bg-dark-grey mx-auto pl-6"
+        value={arraySize}
+        onChange={(event) => setArraySize(event.target.value)}
+        />
+      <button onClick={handleClick} className="pl-6">Enter</button>
+      {/* GRID DISPLAY */}
+      <canvas 
+        className="w-[500px] h-[500px] bg-black" 
+        ref={canvasRef}>
+
+      </canvas>
+
+
     </div>
   );
 }
